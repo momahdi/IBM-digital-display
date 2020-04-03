f3c43cc81f873b13e2670b02a507d088e102bdbb
momahdi <mohamed4mahdi@gmail.com>
2020-04-03 14:21:36 +0200

    prototype

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-
 import './App.css';
 
+
 function App() {
+  const text = "This is a test message that is provided to give an idea about the final result "
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>Prototype Display</p>
+        <div className="screen">
+        <div id="text">{text}</div>
+        </div>
       </header>
     </div>
   );
