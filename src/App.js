74f7d47bb638918d871dddb88e3b2488b6172d8c
momahdi <mohamed4mahdi@gmail.com>
2020-04-03 14:44:57 +0200

    prototype2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 
 function App() {
-  const text = "This is a test message that is provided to give an idea about the final result "
+  const text = "This is a test message that is provided to give an idea about the final result! "
   return (
     <div className="App">
       <header className="App-header">
